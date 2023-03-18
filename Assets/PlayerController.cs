118166714cb4a5071e91b11c811b650ad413d60b
michaelhly <michaelhly@gmail.com>
2023-03-17 23:41:22 -0400

    Use right hand joystick as input

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,11 +11,20 @@ public class PlayerController : MonoBehaviour
     // Rigidbody variable to hold the player ball's rigidbody instance
     private Rigidbody rb;
 
+    private InputDevice targetDevice;
+
     // Called before the first frame update
     void Start()
     {
         // Assigns the player ball's rigidbody instance to the variable
         rb = GetComponent<Rigidbody>();
+        List<InputDevice> devices = new List<InputDevice>();
+        InputDevices.GetDevicesAtXRNode(XRNode.RightHand, devices);
+
+        if (devices.Count > 0)
+        {
+            targetDevice = devices[0];
+        }
     }
 
     // Called once per frame
@@ -22,9 +32,10 @@ public class PlayerController : MonoBehaviour
     {
         // The float variables, moveHorizontal and moveVertical, holds the value of the virtual axes, X and Z.
 
-        // It records input from the keyboard.
-        float moveHorizontal = Input.GetAxis("Horizontal");
-        float moveVertical = Input.GetAxis("Vertical");
+        // It records input from the XR controller.
+        targetDevice.TryGetFeatureValue(CommonUsages.primary2DAxis, out Vector2 primary2DAxisValue);
+        float moveHorizontal = primary2DAxisValue.x;
+        float moveVertical = primary2DAxisValue.y;
 
         // Vector3 variable, movement, holds 3D positions of the player ball in form of X, Y, and Z axes in the space.
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
